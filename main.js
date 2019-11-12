c7886219709e673a02d2b17825f450791d3a8998
Michael Torres <michaelj.torres@yahoo.com>
2019-11-11 21:42:07 -0500

    input works

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
 const toDoList = [];
 
-let remove =  (index) => {
-    this.index.splice(index, 1);
-}
+function inputText() {
+   let textData = document.getElementById("input").value; 
+   document.getElementById("list").innerHTML = textData;
+ 
+   console.log('working')
+  }
+
+
+
+// document.getElementById("remove").addEventListener("click", function () {
+//     let remove = (index) => {
+//         this.index.splice(index, 1);
+//     }
+//   }, false);
+
+
