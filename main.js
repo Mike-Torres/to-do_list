89eb13815a3d4b1c674ab91a58abccb303a89fb3
Michael Torres <michaelj.torres@yahoo.com>
2019-11-12 21:21:36 -0500

    appendchild and create node added

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 const toDoList = [];
 
 function inputText() {
-   let textData = document.getElementById("input").value; 
-   document.getElementById("list").innerHTML = textData;
- 
+   var item = document.getElementById("input").value; 
+   var text = document.createTextNode(item);
+   var newItem = document.createElement('li')
+   newItem.appendChild(text);
+   document.getElementById('list').appendChild(newItem)
+   
    console.log('working')
   }
 
