90354f8870b7bc0301d4f84c245458511ee08ded
Michael Torres <michaelj.torres@yahoo.com>
2019-11-16 13:06:04 -0500

    creating enter button for input

--- main.js
+++ main.js
@@ -1,18 +1,25 @@
 const toDoList = [];
 
 function inputText() {
-   var item = document.getElementById("input").value; 
-   var text = document.createTextNode(item);
-   var newItem = document.createElement('li')
-   newItem.appendChild(text);
-   document.getElementById('list').appendChild(newItem)
-   
-   console.log('working')
-  }
+  var item = document.getElementById("input").value;   //input value of what's being typed in
+  var text = document.createTextNode(item);   // storing the input value from Item into text
+  var newItem = document.createElement('li')  //  displaying (item) on a list 
+  newItem.appendChild(text);  //input value stored in text is being attached to list item where it will be displayed
+  document.getElementById('list').appendChild(newItem) //seperate entity... this is where the source of truth is actually being displayed
+  
+  console.log('working')
+ }
 
 
 function removeText() {
-    // Step 2 
+  // var list = document.getElementById('list')
+  // list.removeChild(list.childNodes[0])
+
+  var item = document.getElementById("input").value; 
+  var text = document.createTextNode(item);
+  var newItem = document.createElement('li')
+  
+  newItem.removeChild(text.lastElementChild)
 }
 
 
