b9d88479429f1d62cd3a197ec3db527ae6caca3a
Michael Torres <michaelj.torres@yahoo.com>
2019-11-17 19:27:15 -0500

    enter button and click button both function correctly

--- main.js
+++ main.js
@@ -3,9 +3,9 @@ const toDoList = [];
 document.addEventListener("keydown", keyDownEnter, false);
 
 function keyDownEnter(e) {
-  if(e.keyCode == 13) {
-  alert("You hit the enter key.");
-  console.log('Hey')
+  if(e.keyCode === 13) {
+    event.preventDefault();
+    inputText();
   } 
 }
 
@@ -15,8 +15,6 @@ function inputText() {
   var newItem = document.createElement('li')  //  displaying (item) on a list 
   newItem.appendChild(text);  //input value stored in text is being attached to list item where it will be displayed
   document.getElementById('list').appendChild(newItem) //seperate entity... this is where the source of truth is actually being displayed
-  
-  console.log('working')
  }
 
 function removeText() {
