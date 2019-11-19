21ae90b9d2eb0820525f9f50f222056cdec85c7f
Michael Torres <michaelj.torres@yahoo.com>
2019-11-18 21:33:44 -0500

    adding delete Fa Fa trash to li

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ function inputText() {
   var item = document.getElementById("input").value;   //input value of what's being typed in
   var text = document.createTextNode(item);   // storing the input value from Item into text
   var newItem = document.createElement('li')  //  displaying (item) on a list 
-  newItem.appendChild(text);  //input value stored in text is being attached to list item where it will be displayed
+  newItem.appendChild(text) //input value stored in text is being attached to list item where it will be displayed
   document.getElementById('list').appendChild(newItem) //seperate entity... this is where the source of truth is actually being displayed
  }
 
