cf75e53fff906eef97e4fd0151b3ae65cc285e97
Michael Torres <michaelj.torres@yahoo.com>
2019-11-19 22:27:56 -0500

    fa fa trash added, now I need to put it to end of LI

--- main.js
+++ main.js
@@ -3,28 +3,30 @@ const toDoList = [];
 document.addEventListener("keydown", keyDownEnter, false);
 
 function keyDownEnter(e) {
-  if(e.keyCode === 13) {
+  if (e.keyCode === 13) {
     event.preventDefault();
     inputText();
-  } 
+  }
 }
 
 function inputText() {
-  var item = document.getElementById("input").value;   //input value of what's being typed in
-  var text = document.createTextNode(item);   // storing the input value from Item into text
-  var newItem = document.createElement('li')  //  displaying (item) on a list 
+  var item = document.getElementById("input").value; //input value of what's being typed in
+  var text = document.createTextNode(item); // storing the input value from Item into text
+  var newItem = document.createElement('li') //  displaying (item) on a list 
   newItem.appendChild(text) //input value stored in text is being attached to list item where it will be displayed
   document.getElementById('list').appendChild(newItem) //seperate entity... this is where the source of truth is actually being displayed
- }
+
+  const trashCan = document.createElement('i')
+  trashCan.classList.add("fa", "fa-trash")
+  newItem.appendChild(trashCan)
+}
 
 
 function removeText() {
   var list = document.getElementsByTagName("li"); // or document.querySelectorAll("li"); 
-  for(var i = 0; i < list.length; i++) {
-    list[i].onclick = function() {
+  for (var i = 0; i < list.length; i++) {
+    list[i].onclick = function () {
       this.parentNode.removeChild(this);
     }
   }
 }
-
-
